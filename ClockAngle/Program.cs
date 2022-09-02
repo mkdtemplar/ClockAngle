a8a076565a36af2a7bf2fb3c4f411ef36c50e2cc
Ivan Markovski <sagitariusim@gmail.com>
2022-09-02 12:46:41 +0200

    Updated as requested

--- ClockAngle/Program.cs
+++ ClockAngle/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-class ClockAngle
-{
-    static double calculateAngle(int h, int m)
-    {
-        // validate the input
-        if (h < 0 || m < 0 || h > 12 || m > 60)
-        {
-            Console.WriteLine("Wrong input");
-        }
-            
+﻿using System.ComponentModel;
 
+namespace ClockAngle;
+using static Console;
+
+internal static class Program
+{
+    static double CalculateAngle(int h, int m)
+    {
         if (h == 12)
         {
             h = 0;
@@ -43,13 +40,70 @@ class ClockAngle
 
     public static void Main()
     {
+        int hours;
+        int minute;
 
-        Console.Write("Enter hour: ");
-        int hours = Convert.ToInt32(Console.ReadLine());
+        Write("Enter hour: ");
+        string input = Console.ReadLine() ?? throw new InvalidOperationException();
+        
+        bool intResultHour = int.TryParse(input, out hours);
 
-        Console.Write("Enter minutes: ");
-        int minute = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine(calculateAngle(hours, minute));
+        while (intResultHour == false)
+        {
+            WriteLine("Input for hours is not in integer format!");
+            Write("Enter hour: ");
+            input = ReadLine() ?? throw new InvalidOperationException();
+            intResultHour = int.TryParse(input, out hours);
+        }
+
+        while ((hours is < 0 or > 12) || intResultHour == false)
+        {
+            if (hours is < 0 or > 12)
+            {
+                WriteLine("Hours must be greater than 0 and less or equal than 12!");
+            }
+
+            if (intResultHour == false)
+            {
+                WriteLine("Input for hours is not in integer format!");
+            }
+
+            Write("Enter hour: ");
+            input = ReadLine() ?? throw new InvalidOperationException();
+            intResultHour = int.TryParse(input, out hours);
+        }
+
+        Write("Enter minutes: ");
+        input = ReadLine() ?? throw new InvalidOperationException();
+
+        bool intResultMinute = int.TryParse(input, out minute);
+
+        while (intResultMinute == false)
+        {
+            WriteLine("Input for minutes is not in integer format");
+            Write("Enter minutes: ");
+            input = ReadLine() ?? throw new InvalidOperationException();
+            intResultMinute = int.TryParse(input, out minute);
+        }
+
+        while ((minute is < 0 or > 60) || intResultMinute == false)
+        {
+            if (minute is < 0 or > 60)
+            {
+                WriteLine("Minutes must be greater than 0 and less or equal to 60!");
+            }
+
+            if (intResultMinute == false)
+            {
+                WriteLine("Input for minutes is not in integer format");
+            }
+
+            Write("Enter minutes: ");
+            input = ReadLine() ?? throw new InvalidOperationException();
+            intResultMinute = int.TryParse(input, out minute);
+        }
+
+        WriteLine(CalculateAngle(hours, minute));
     }
 }
